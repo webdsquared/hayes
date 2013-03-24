6746283ba8bae749ca8e713bad9de8d6f42072bb
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-03-23 23:04:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hayes::Application.config.secret_token = '9b15b0bf178665c5fab998aa6c1ecc73cadd021286c31d84371afa5788122a4d8a9a5c7d75c1cb8b4daa6d6478461115cc41e70d81f506f14b157e27dfc37124'
